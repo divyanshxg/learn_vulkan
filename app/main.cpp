3f0ab5eb248ea7d6824a4a0f75855b33fea3163a
Divyansh Gupta <divyanshxg@gmail.com>
2026-09-24 22:25:50 +0530

    swap chain

--- app/main.cpp
+++ app/main.cpp
@@ -14,6 +14,7 @@
 #else
 import vulkan_hpp;
 #endif
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <cstdlib>
@@ -59,6 +60,11 @@ private:
   vk::raii::Device device = nullptr;
   vk::raii::Queue graphicsQueue = nullptr;
   vk::raii::SurfaceKHR surface = nullptr;
+  vk::raii::SwapchainKHR swapChain = nullptr;
+  vk::SurfaceFormatKHR swapChainSurfaceFormat;
+  vk::Extent2D swapChainExtent;
+  std::vector<vk::raii::ImageView> swapChainImageViews;
+  std::vector<vk::Image> swapChainImages;
 
   void initWindow() {
     glfwInit();
@@ -163,6 +169,91 @@ private:
 
     pickPhysicalDevice();
     createLogicalDevice();
+    createSwapChain();
+  }
+
+  void createSwapChain() {
+    vk::SurfaceCapabilitiesKHR surfaceCapabilities =
+        physicalDevice.getSurfaceCapabilitiesKHR(*surface);
+    swapChainExtent = chooseSwapExtent(surfaceCapabilities);
+    uint32_t minImageCount = chooseSwapMinImageCount(surfaceCapabilities);
+
+    std::vector<vk::SurfaceFormatKHR> availableFormats =
+        physicalDevice.getSurfaceFormatsKHR(*surface);
+    swapChainSurfaceFormat = chooseSwapSurfaceFormat(availableFormats);
+
+    std::vector<vk::PresentModeKHR> availablePresentModes =
+        physicalDevice.getSurfacePresentModesKHR(*surface);
+    vk::PresentModeKHR presentMode =
+        chooseSwapPresentMode(availablePresentModes);
+
+    vk::SwapchainCreateInfoKHR swapChainCreateInfo{
+        .surface = *surface,
+        .minImageCount = minImageCount,
+        .imageFormat = swapChainSurfaceFormat.format,
+        .imageColorSpace = swapChainSurfaceFormat.colorSpace,
+        .imageExtent = swapChainExtent,
+        .imageArrayLayers = 1,
+        .imageUsage = vk::ImageUsageFlagBits::eColorAttachment,
+        .imageSharingMode = vk::SharingMode::eExclusive,
+        .preTransform = surfaceCapabilities.currentTransform,
+        .compositeAlpha = vk::CompositeAlphaFlagBitsKHR::eOpaque,
+        .presentMode = presentMode,
+        .clipped = true};
+
+    swapChain = vk::raii::SwapchainKHR(device, swapChainCreateInfo);
+    swapChainImages = swapChain.getImages();
+  }
+
+  static uint32_t chooseSwapMinImageCount(
+      vk::SurfaceCapabilitiesKHR const &surfaceCapabilities) {
+    auto minImageCount = std::max(3u, surfaceCapabilities.minImageCount);
+    // maxImageCount == 0 means unlimited images
+    if ((0 < surfaceCapabilities.maxImageCount) &&
+        (surfaceCapabilities.maxImageCount < minImageCount)) {
+      minImageCount = surfaceCapabilities.maxImageCount;
+    }
+    return minImageCount;
+  }
+
+  static vk::SurfaceFormatKHR chooseSwapSurfaceFormat(
+      std::vector<vk::SurfaceFormatKHR> const &availableFormats) {
+    assert(!availableFormats.empty());
+    const auto formatIt =
+        std::ranges::find_if(availableFormats, [](const auto &format) {
+          return format.format == vk::Format::eB8G8R8A8Srgb &&
+                 format.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear;
+        });
+    return formatIt != availableFormats.end() ? *formatIt : availableFormats[0];
+  }
+
+  static vk::PresentModeKHR chooseSwapPresentMode(
+      std::vector<vk::PresentModeKHR> const &availablePresentModes) {
+    assert(std::ranges::any_of(availablePresentModes, [](auto presentMode) {
+      return presentMode == vk::PresentModeKHR::eFifo;
+    }));
+    return std::ranges::any_of(availablePresentModes,
+                               [](const vk::PresentModeKHR value) {
+                                 return vk::PresentModeKHR::eMailbox == value;
+                               })
+               ? vk::PresentModeKHR::eMailbox
+               : vk::PresentModeKHR::eFifo;
+  }
+
+  vk::Extent2D
+  chooseSwapExtent(vk::SurfaceCapabilitiesKHR const &capabilities) {
+    // this condition means does vulkan know the size of swapchain
+    if (capabilities.currentExtent.width !=
+        std::numeric_limits<uint32_t>::max()) {
+      return capabilities.currentExtent;
+    }
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+
+    return {std::clamp<uint32_t>(width, capabilities.minImageExtent.width,
+                                 capabilities.maxImageExtent.width),
+            std::clamp<uint32_t>(height, capabilities.minImageExtent.height,
+                                 capabilities.maxImageExtent.height)};
   }
 
   void createSurface() {
